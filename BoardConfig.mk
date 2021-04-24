5ca26b557aef20d95dd4aa6258c0786cc162bcd1
I-n-o-k <inok.dr189@gmail.com>
2021-04-24 08:54:25 +0000

    juice: add libinit

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -69,6 +69,10 @@ TARGET_ENABLE_MEDIADRM_64 := true
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 
+# Init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_juice
+TARGET_RECOVERY_DEVICE_MODULES := libinit_juice
+
 # kernel
 BOARD_KERNEL_CMDLINE := \
     console=ttyMSM0,115200n8 \
